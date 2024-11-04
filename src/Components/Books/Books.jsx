9a90ce147dd3a176b88dba8ce4e0135178659886
Md. Rabiul Islam <rabiul77777888@gmail.com>
2024-11-04 20:14:27 +0600

    load fake data to display books

---
+++ src/Components/Books/Books.jsx
@@ -0,0 +1,32 @@
+import { data } from 'autoprefixer';
+import React, { useEffect, useState } from 'react';
+
+const Books = () => {
+ 
+    const [books, setBooks] = useState([]);
+
+    useEffect( () =>{
+        fetch('../../../public/booksData.json')
+        .then(res => res.json())
+        .then(data => setBooks(data))
+    } ,[])
+    return (
+        <div>
+           <h2 className="text-4xl font-bold text-center">Books</h2>
+           <p>{books.length}</p>
+        </div>
+    );
+};
+
+export default Books;
+
+
+/**
+ * 1. state to store books
+ * 2. useEffect
+ * 3. fetch to load data
+ * 4. set the data to the books state
+ * 
+ * 
+ * 
+ */
